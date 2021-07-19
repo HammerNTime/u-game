d0c557ea444442809592a06999263fd473e3c73d
HammerNTime <hammer.dylan@gmail.com>
2021-07-19 11:00:51 -0400

    add index view

--- controllers/index.js
+++ controllers/index.js
@@ -14,13 +14,14 @@ function index(req, res) {
   }
   Game.find({})
   .then(games => {
-    let randNum1 = null
-    let randNum2 = null
-    let randNum3 = null
-    randNum1 = getRandomInt(games.length)
+    let randNums = getRandomInt(games.length)
+    console.log(randNums)
     res.render('index', { 
       title: 'Home', 
       user: req.user ? req.user : null,
+      game1: games[randNums[0]],
+      game2: games[randNums[1]],
+      game3: games[randNums[2]]
       
     })
   })
@@ -28,5 +29,17 @@ function index(req, res) {
 
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max)
+  let nums = []
+  let num1 = Math.floor(Math.random() * max)
+  let num2 = Math.floor(Math.random() * max)
+  let num3 = Math.floor(Math.random() * max)
+  while (num2 === num1) {
+    num2 = Math.floor(Math.random() * max)
+  }
+  while (num3 === num1 || num3 === num2) {
+    num3 = Math.floor(Math.random() * max)
+  }
+  nums.push(num1, num2, num3)
+  console.log(nums)
+  return nums
 }
